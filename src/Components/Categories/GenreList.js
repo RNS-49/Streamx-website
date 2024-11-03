58e89b03699129087a6fea5dd09fe8fc2bb46c52
RNS-49 <renjithns121@gmail.com>
2024-11-03 12:32:57 +0530

    first commit

--- src/Components/Categories/GenreList.js
+++ src/Components/Categories/GenreList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './categories.css'
 
 function GenreList() {
   return (
@@ -6,95 +7,95 @@ function GenreList() {
       <section className='genre container-fluid'>
         <h1>Genres</h1>
         <div className='row justify-content-around'>
-          <div className='col-lg-2 col-sm-4'>
+          <div className='col-lg-2 col-sm-4 genre-card'>
             <h5>Action</h5>
           </div>
-          <div className='col-lg-2 col-sm-4'>
+          <div className='col-lg-2 col-sm-4 genre-card'>
             <h5>Adventure</h5>
           </div>
-          <div className='col-lg-2 col-sm-4'>
+          <div className='col-lg-2 col-sm-4 genre-card'>
             <h5>Comedy</h5>
           </div>
-          <div className='col-lg-2 col-sm-4'>
+          <div className='col-lg-2 col-sm-4 genre-card'>
             <h5>Drama</h5>
           </div>
-          <div className='col-lg-2 col-sm-4'>
+          <div className='col-lg-2 col-sm-4 genre-card'>
             <h5>Horror</h5>
           </div>
-          <div className='col-lg-2 col-sm-4'>
+          <div className='col-lg-2 col-sm-4 genre-card'>
             <h5>Sci-Fi</h5>
           </div>
         
-          <div className='col-lg-2 col-sm-4'>
+          <div className='col-lg-2 col-sm-4 genre-card'>
             <h5>Fantacy</h5>
           </div>
-          <div className='col-lg-2 col-sm-4'>
+          <div className='col-lg-2 col-sm-4 genre-card'>
             <h5>Thriller</h5>
           </div>
-          <div className='col-lg-2 col-sm-4'>
+          <div className='col-lg-2 col-sm-4 genre-card'>
             <h5>Mystery</h5>
           </div>
-          <div className='col-lg-2 col-sm-4'>
+          <div className='col-lg-2 col-sm-4 genre-card'>
             <h5>Romance</h5>
           </div>
-          <div className='col-lg-2 col-sm-4'>
+          <div className='col-lg-2 col-sm-4 genre-card'>
             <h5>Documentary</h5>
           </div>
-          <div className='col-lg-2 col-sm-4'>
+          <div className='col-lg-2 col-sm-4 genre-card'>
             <h5>Animation</h5>
           </div>
-          <div className='col-lg-2 col-sm-4'>
+          <div className='col-lg-2 col-sm-4 genre-card'>
             <h5>Crime</h5>
           </div>
-          <div className='col-lg-2 col-sm-4'>
+          <div className='col-lg-2 col-sm-4 genre-card'>
             <h5>Western</h5>
           </div>
-          <div className='col-lg-2 col-sm-4'>
+          <div className='col-lg-2 col-sm-4 genre-card'>
             <h5>Historical</h5>
           </div>
-          <div className='col-lg-2 col-sm-4'>
+          <div className='col-lg-2 col-sm-4 genre-card'>
             <h5>Biographical</h5>
           </div>
-          <div className='col-lg-2 col-sm-4'>
+          <div className='col-lg-2 col-sm-4 genre-card'>
             <h5>War</h5>
           </div>
-          <div className='col-lg-2 col-sm-4'>
+          <div className='col-lg-2 col-sm-4 genre-card'>
             <h5>Family</h5>
           </div>
-          <div className='col-lg-2 col-sm-4'>
+          <div className='col-lg-2 col-sm-4 genre-card'>
             <h5>Sports</h5>
           </div>
-          <div className='col-lg-2 col-sm-4'>
+          <div className='col-lg-2 col-sm-4 genre-card'>
             <h5>Noir</h5>
           </div>
-          <div className='col-lg-2 col-sm-4'>
+          <div className='col-lg-2 col-sm-4 genre-card'>
             <h5>Psychological</h5>
           </div>
-          <div className='col-lg-2 col-sm-4'>
+          <div className='col-lg-2 col-sm-4 genre-card'>
             <h5>Coming-of-Age</h5>
           </div>
-          <div className='col-lg-2 col-sm-4'>
+          <div className='col-lg-2 col-sm-4 genre-card'>
             <h5>Fantacy Comedy</h5>
           </div>
-          <div className='col-lg-2 col-sm-4'>
+          <div className='col-lg-2 col-sm-4 genre-card'>
             <h5>Epic</h5>
           </div>
-          <div className='col-lg-2 col-sm-4'>
+          <div className='col-lg-2 col-sm-4 genre-card'>
             <h5>Superhero</h5>
           </div>
-          <div className='col-lg-2 col-sm-4'>
+          <div className='col-lg-2 col-sm-4 genre-card'>
             <h5>Surreal</h5>
           </div>
-          <div className='col-lg-2 col-sm-4'>
+          <div className='col-lg-2 col-sm-4 genre-card'>
             <h5>Experimental</h5>
           </div>
-          <div className='col-lg-2 col-sm-4'>
+          <div className='col-lg-2 col-sm-4 genre-card'>
             <h5>Political</h5>
           </div>
-          <div className='col-lg-2 col-sm-4'>
+          <div className='col-lg-2 col-sm-4 genre-card'>
             <h5>Post-Apocalyptic</h5>
           </div>
-          <div className='col-lg-2 col-sm-4'>
+          <div className='col-lg-2 col-sm-4 genre-card'>
             <h5>K-drama</h5>
           </div>
          <h6 className='sign'>SignIn or SignUp</h6>
